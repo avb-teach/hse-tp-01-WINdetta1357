12cd5a24b8158e79b1a6f18c69d5a2fbf147d601
Mark Gurshtein <markgurshtein@MacBook-Air-Mark.local>
2025-04-25 02:28:53 +0300

    9

--- collect_files.py
+++ collect_files.py
@@ -1,12 +1,10 @@
 import sys
-import os
 
-def pushh(input_dir, output_dir):
-    files = os.listdir(input_dir)
-    for file in files:
-        print(file)
+def collect_files(input_dir, output_dir):
+    print(input_dir, output_dir)
 
 if __name__ == "__main__":
     input_dir = sys.argv[1]
     output_dir = sys.argv[2]
-    pushh(input_dir, output_dir)
+    collect_files(input_dir, output_dir)
+
