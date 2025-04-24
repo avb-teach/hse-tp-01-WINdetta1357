c6a5ce3bdcaa57f30d2e988c20226da5ce58be1c
Mark Gurshtein <markgurshtein@MacBook-Air-Mark.local>
2025-04-25 02:02:31 +0300

    3

---
+++ pythh.py
@@ -0,0 +1,9 @@
+import sys
+
+def collect_files(input_dir, output_dir):
+    pass
+
+if __name__ == "__main__":
+    input_dir = sys.argv[1]
+    output_dir = sys.argv[2]
+    collect_files(input_dir, output_dir)
