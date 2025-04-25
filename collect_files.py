a858cde1f1bd82d09cbb63286d5db7a789be2dc2
Mark Gurshtein <markgurshtein@MacBook-Air-Mark.local>
2025-04-26 01:44:47 +0300

    устав

--- collect_files.py
+++ collect_files.py
@@ -1,10 +1,10 @@
 import os
-import shutil
 import sys
+import shutil
 
 def f(i, o, m=None):
     d = {}
-    
+
     def u(p, fn):
         if p not in d:
             d[p] = {}
@@ -15,8 +15,10 @@ def f(i, o, m=None):
         else:
             d[p][fn] = 1
             return fn
-    
+
     def r(c, rel):
+        depth = len(rel.split(os.sep)) if rel else 0
+
         if m is None:
             tgt = o
         else:
@@ -24,39 +26,50 @@ def f(i, o, m=None):
                 tgt = os.path.join(o, rel)
             else:
                 tgt = o
+
         if not os.path.exists(tgt):
             os.makedirs(tgt)
-        
+
         if rel:
             rl = rel.split(os.sep)
         else:
             rl = []
-        
+
         for x in os.scandir(c):
             if x.is_file():
                 n = x.name
                 nn = u(tgt, n)
                 t = os.path.join(tgt, nn)
                 shutil.copy(x.path, t)
+
                 if m is not None:
                     if len(rl) >= m:
                         if m > 1:
                             new_rel = os.path.join(*rl[-(m - 1):])
                         else:
                             new_rel = ""
+
                         if new_rel:
                             lift_dir = os.path.join(o, new_rel)
                         else:
                             lift_dir = o
+
                         if not os.path.exists(lift_dir):
                             os.makedirs(lift_dir)
+
                         nn2 = u(lift_dir, n)
                         lt = os.path.join(lift_dir, nn2)
                         shutil.copy(x.path, lt)
+
             else:
                 if x.is_dir():
-                    nr = os.path.join(rel, x.name) if rel else x.name
-                    r(x.path, nr)
+                    if m is None or depth < m:
+                        if rel:
+                            nr = os.path.join(rel, x.name)
+                        else:
+                            nr = x.name
+
+                        r(x.path, nr)
 
     r(i, "")
 
@@ -64,15 +77,15 @@ if __name__ == "__main__":
     if len(sys.argv) < 3:
         print("Usage: python collect_files.py <input_dir> <output_dir> [max_depth]")
         sys.exit(1)
-    
+
     ii = sys.argv[1]
     oo = sys.argv[2]
     mm = None
-    
+
     if len(sys.argv) > 3:
         try:
             mm = int(sys.argv[3])
         except:
             mm = None
-    
+
     f(ii, oo, mm)
